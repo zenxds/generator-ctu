c9a11da484fd904db746f88bac7eeab5e23c7ea3
zenxds <zenxds@gmail.com>
2017-06-29 10:25:49 +0800

    修改提示

--- generators/app/index.js
+++ generators/app/index.js
@@ -83,7 +83,7 @@ class CTU extends Generator {
   }
 
   install() {
-    this.log('正在下载项目依赖'.green)    
+    this.log('正在安装项目依赖'.green)    
     this.spawnCommandSync('npm', ['install', '--registry=https://registry.npm.taobao.org'])
     this.log('正在启动项目'.green)
     this.spawnCommandSync('npm', ['start'])
