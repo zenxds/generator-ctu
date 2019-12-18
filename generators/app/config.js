123700787554286ba91fd18a5391436aa6bb2e5e
zenxds <zenxds@gmail.com>
2019-12-18 13:43:01 +0800

    调整配置顺序

--- generators/app/config.js
+++ generators/app/config.js
@@ -1,16 +1,6 @@
 module.exports = {
   projects: [
     {
-      name: "es6-boilerplate",
-      description: "ES6",
-      repository: "https://github.com/zenxds/es6-boilerplate/archive/master.tar.gz"
-    },
-    {
-      name: "react-boilerplate",
-      description: "React",
-      repository: "https://github.com/zenxds/react-boilerplate/archive/master.tar.gz"
-    },
-    {
       name: "react-spa-boilerplate",
       description: "React SPA",
       repository: "https://github.com/zenxds/react-spa-boilerplate/archive/master.tar.gz"
@@ -19,6 +9,27 @@ module.exports = {
       name: "nightkay-app-boilerplate",
       description: "nightKay App",
       repository: "https://github.com/zenxds/nightkay-app-boilerplate/archive/master.tar.gz"
+    },
+    {
+      name: "boilerplate-component-gallery",
+      description: "组件库",
+      repository: "https://github.com/gafish/boilerplate-component-gallery/archive/master.tar.gz"
+    },
+    {
+      name: "boilerplate-simple-component",
+      description: "组件库组件",
+      mode: "simple",
+      repository: "https://github.com/gafish/boilerplate-simple-component/archive/master.tar.gz"
+    },
+    {
+      name: "node-lib-boilerplate",
+      description: "Node Lib",
+      repository: "https://github.com/zenxds/node-lib-boilerplate/archive/master.tar.gz"
+    },
+    {
+      name: "react-lib-boilerplate",
+      description: "React Lib",
+      repository: "https://github.com/zenxds/react-lib-boilerplate/archive/master.tar.gz"
     },
     {
       name: "h5-boilerplate",
@@ -36,25 +47,14 @@ module.exports = {
       repository: "https://github.com/zenxds/koa-boilerplate/archive/master.tar.gz"
     },
     {
-      name: "node-lib-boilerplate",
-      description: "Node Lib",
-      repository: "https://github.com/zenxds/node-lib-boilerplate/archive/master.tar.gz"
+      name: "es6-boilerplate",
+      description: "ES6",
+      repository: "https://github.com/zenxds/es6-boilerplate/archive/master.tar.gz"
     },
     {
-      name: "react-lib-boilerplate",
-      description: "React Lib",
-      repository: "https://github.com/zenxds/react-lib-boilerplate/archive/master.tar.gz"
-    },
-    {
-      name: "boilerplate-component-gallery",
-      description: "组件库",
-      repository: "https://github.com/gafish/boilerplate-component-gallery/archive/master.tar.gz"
-    },
-    {
-      name: "boilerplate-simple-component",
-      description: "组件库组件",
-      mode: "simple",
-      repository: "https://github.com/gafish/boilerplate-simple-component/archive/master.tar.gz"
+      name: "react-boilerplate",
+      description: "React",
+      repository: "https://github.com/zenxds/react-boilerplate/archive/master.tar.gz"
     }
   ]
 }
